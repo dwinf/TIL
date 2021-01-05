f6c3e8e52ab55a525ec3f3c513f5a12010f345f5
DaeChan Kim <dwinf88@gmail.com>
2021-01-05 20:26:08 +0900

    python day 2 practice

---
+++ 파이썬/실습파일/conditionLab2.py
@@ -0,0 +1,5 @@
+color_name = input("컬러 하나를 입력하세요: ")
+if color_name == 'red':
+    print("#ff0000")
+else:
+    print("#000000")
---
+++ 파이썬/실습파일/opertest2.py
@@ -0,0 +1,10 @@
+print(1 % 2, 2 % 2, 3 % 2, 4 % 2)
+num = 100
+print(num)
+num = num + 10
+print(num)
+num += 10
+print(num)
+
+age = 20
+not(age == 10)
