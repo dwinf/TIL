f3c2b316586274683e7ae3dbcf916b3c182e94a4
DaeChan Kim <dwinf88@gmail.com>
2021-01-20 20:31:41 +0900

    python day12

---
+++ 파이썬/day12/importall.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("C:/PyStudy")
+
+from mypack.calc import *
+add.outadd(1,2)
+multi.outmulti(1,2)
+
+
+'''from mypack.report.table import *
+outreport()
+a()
+b()
+c()'''
---
+++ 파이썬/day12/usepackage.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append("C:/PyStudy")
+print(sys.path)
+
+import mypack.calc.add
+mypack.calc.add.outadd(1,2)
+
+import mypack.report.table
+mypack.report.table.outreport()
+
+import mypack.calc.add
+mypack.calc.add.outadd(1,2)
+
+import mypack.report.table
+mypack.report.table.outreport()
